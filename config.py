3e10e00bf830ff6be0a3f78b378786a1416e2892
Ryoumi <ddaloisi@emich.edu>
2020-11-02 00:48:14 +0000

    updated directories

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-my_api_key = 'AIzaSyCW7MEHACsNcMvpodGS06panjCWFXCZe40' #The API_KEY you aquired
-my_cse_id = '05506562c5354ee18' # The search-engine-ID you created
+api_key = 'AIzaSyCW7MEHACsNcMvpodGS06panjCWFXCZe40' #The API_KEY you aquired
+cse_id = '05506562c5354ee18' # The search-engine-ID you created
+
+target_topic = 'Playstation 5'
+google_search_maximum_page_number = 10
