f2f70078464460a23ca3cc5ff96e167639605cab
Ryoumi <ddaloisi@emich.edu>
2020-11-02 00:14:00 +0000

    added app and everything else >

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-my_api_key = '' #The API_KEY you aquired
-my_cse_id = '' # The search-engine-ID you created
+my_api_key = 'AIzaSyCW7MEHACsNcMvpodGS06panjCWFXCZe40' #The API_KEY you aquired
+my_cse_id = '05506562c5354ee18' # The search-engine-ID you created
